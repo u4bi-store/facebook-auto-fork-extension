61c083f2b5fd52044c7d2faf11ffc942d05a0e7c
Yu Myungjae <u4bi@letsee.io>
2017-07-26 16:31:44 +0900

    interval render

--- execute.js
+++ execute.js
@@ -1,8 +1,11 @@
 (function(){
-            
+
+    setInterval(function() {
+        
         var 
             fork = document.querySelectorAll('a._42ft._4jy0._4jy3._4jy1.selected._51sy');
         fork.length ? fork[0].click() : window.location.reload();
-
+        
+    }, 500);
     
 })();
